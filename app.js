fd7845995cd9e5977f39d49582d26cde14233c04
cherno1234 <cherno.github@gmail.com>
2020-09-24 01:39:29 +0000

    add item on Finance module Controller

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var uiController = (function() {
   return {
     getInput: function() {
       return {
-        type: document.querySelector(DOMstrings.inputType).value,
+        type: document.querySelector(DOMstrings.inputType).value, //exp, inc
         description: document.querySelector(DOMstrings.inputDescription).value,
         value: document.querySelector(DOMstrings.inputValue).value
       };
@@ -22,19 +22,22 @@ var uiController = (function() {
 
 //Sankhvvtei ajillakh Controller
 var financeController = (function() {
+  //private function
   var Income = function(id, description, value) {
     this.id = id;
     this.description = description;
     this.value = value;
   };
-
+  //private function
   var Expense = function(id, description, value) {
     this.id = id;
     this.description = description;
     this.value = value;
   };
+
+  //private data
   var data = {
-    allItems: {
+    items: {
       inc: [],
       exp: []
     },
@@ -43,14 +46,37 @@ var financeController = (function() {
       exp: 0
     }
   };
+
+  return {
+    addItem: function(type, desc, val) {
+      //   console.log("item added ....");
+      var item, id;
+      //identification
+      if (data.items[type].length === 0) id = 1;
+      else {
+        id = data.items[type][data.items[type].length - 1].id + 1;
+      }
+
+      if (type === "inc") {
+        item = new Income(id, desc, val);
+      } else {
+        item = new Expense(id, desc, val);
+      }
+      data.items[type].push(item);
+    },
+    seeData: function() {
+      return data;
+    }
+  };
 })();
 
 //Programmin holbogch Controller
 var appController = (function(uiController, financeController) {
   var ctrlAddItem = function() {
     //1. Oruulakh ogogdliig delgetsees olj awna
-    console.log(uiController.getInput());
+    var input = uiController.getInput();
 
+    financeController.addItem(input.type, input.description, input.value);
     //2. Olj awsan ogogdluude sankhuugiin conrollert damjuuulj hadgalna.
     //3.Olj awsan ogogdlvvdiig web dre tohirokh hesegy gargana
     //4. Toswiig tootsoolno

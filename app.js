1181f039b13bde49fbaec0d6f52c31d770109dcd
cherno1234 <cherno.github@gmail.com>
2020-09-26 10:26:58 +0000

    update display

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ var uiController = (function() {
     inputValue: ".add__value",
     addBtn: ".add__btn",
     incomeList: ".income__list",
-    expenseList: ".expenses__list"
+    expenseList: ".expenses__list",
+    tusuvLabel: ".budget__value",
+    incomeLabel: ".budget__income--value",
+    expenseLabel: ".budget__expenses--value",
+    percentageLabel: ".budget__expenses--percentage"
   };
   return {
     getInput: function() {
@@ -33,6 +37,22 @@ var uiController = (function() {
       });
 
       fieldsArr[0].focus();
+    },
+
+    tusviigUzuuleh: function(tusuv) {
+      document.querySelector(DOMstrings.tusuvLabel).textContent = tusuv.tusuv;
+      document.querySelector(DOMstrings.incomeLabel).textContent =
+        tusuv.totalInc;
+      document.querySelector(DOMstrings.expenseLabel).textContent =
+        tusuv.totalExp;
+
+      if (tusuv.huvi !== 0) {
+        document.querySelector(DOMstrings.percentageLabel).textContent =
+          tusuv.huvi + "%";
+      } else {
+        document.querySelector(DOMstrings.percentageLabel).textContent =
+          tusuv.huvi;
+      }
     },
 
     addListItem: function(item, type) {
@@ -159,9 +179,10 @@ var appController = (function(uiController, financeController) {
       //4. Toswiig tootsoolno
       financeController.tusuwTootsooloh();
       //5. Etsiin uldegdel,
-      var tusuw = financeController.tusviigAvah();
+      var tusuv = financeController.tusviigAvah();
       //6. Toswiin tootsoog delgetsend gargana .
-      console.log(tusuw);
+      uiController.tusviigUzuuleh(tusuv);
+      console.log(tusuv);
     }
   };
 
@@ -182,6 +203,12 @@ var appController = (function(uiController, financeController) {
   return {
     init: function() {
       console.log("Application started ...");
+      uiController.tusviigUzuuleh({
+        tusuv: 0,
+        huvi: 0,
+        totalInc: 0,
+        totalExp: 0
+      });
       setupEventListeners();
     }
   };
